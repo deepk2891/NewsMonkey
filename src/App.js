75d3fe1ce6fb68a8ebe67e32e47094b942e7f19f
deepk2891 <deepk2891@gmail.com>
2022-11-19 21:50:35 +0530

    fetching news from newsApi.org

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
 import News from './components/News';
 
 export default class App extends Component {
+  apiKey = process.env.REACT_APP_NEWS_API;
+
   render() {
-    
     return (
       <div>
         <NavBar/>
-        <News/>
+        <News apiKey={this.apiKey} />
         
       </div>
     )
