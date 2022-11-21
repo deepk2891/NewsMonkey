c7a16bbeac45a7af0eea1b17b447b4bf6270d76b
deepk2891 <deepk2891@gmail.com>
2022-11-21 23:10:19 +0530

    Added Categories & propTypes to NewsMonkey

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import News from './components/News';
 export default class App extends Component {
   apiKey = process.env.REACT_APP_NEWS_API;
 
-  render() {
+  render () {
     return (
       <div>
-        <NavBar/>
-        <News apiKey={this.apiKey} />
-        
+        <NavBar />
+        <News pageSize={ 8 } apiKey={ this.apiKey } country="in" category="sports" />
+
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render () {
-        const { title, description, imageUrl, newsUrl } = this.props
+        const { title, description, imageUrl, newsUrl, author, date } = this.props
         return (
             <div className='my-3'>
-                <div className="card" style={ { width: "18rem" } }>
+                <div className="card">
                     <img src={ !imageUrl ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG84WYLYuZK7_zwWI6wmUdm-xrGeI7dDaSRMftvVsk4l8UObpMSeDsVdou5Hd7Cy7tA2M&usqp=CAU" : imageUrl } className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{ title }</h5>
                         <p className="card-text">{ description }</p>
+                        <p className="card-text"><small className="text-muted">By { !author ? "Unknown" : author } on { date }</small></p>
                         <a target="_blank" rel="noreferrer" href={ newsUrl } className="btn btn-sm  btn-dark">Read more</a>
                     </div>
                 </div>
