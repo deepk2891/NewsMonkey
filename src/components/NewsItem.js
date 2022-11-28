514e90254305de352fcd24b6abd486ac3ad8da8b
deepk2891 <deepk2891@gmail.com>
2022-11-28 09:07:01 +0530

    Added infinite scroll to NewsMonkey

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render () {
-        const { title, description, imageUrl, newsUrl, author, date } = this.props
+        const { title, description, imageUrl, newsUrl, author, date, source } = this.props
         return (
-            <div className='my-3'>
+            <div className="my-3">
                 <div className="card">
+                    <div style={ {
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }>
+                        <span className="badge rounded-pill bg-danger"> { source } </span>
+                    </div>
                     <img src={ !imageUrl ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG84WYLYuZK7_zwWI6wmUdm-xrGeI7dDaSRMftvVsk4l8UObpMSeDsVdou5Hd7Cy7tA2M&usqp=CAU" : imageUrl } className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{ title }</h5>
+                        <h5 className="card-title">{ title }  </h5>
                         <p className="card-text">{ description }</p>
-                        <p className="card-text"><small className="text-muted">By { !author ? "Unknown" : author } on { date }</small></p>
-                        <a target="_blank" rel="noreferrer" href={ newsUrl } className="btn btn-sm  btn-dark">Read more</a>
+                        <p className="card-text"><small className="text-muted">By { !author ? "Unknown" : author } on  { new Date( date ).toGMTString() }</small></p>
+                        <a rel="noreferrer" href={ newsUrl } target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,7 +4,7 @@ import loading from './loading.gif'
 const Spinner = () => {
     return (
         <div className="text-center">
-            <img className="my-3" src={loading} alt="loading" />
+            <img className="my-3" src={ loading } alt="loading" />
         </div>
     )
 }
